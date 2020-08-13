f916ebeeb1263fc196e6b6e834555a6880b318e8
Nick Saxton <nick.saxton@gmail.com>
2020-08-12 22:03:41 -0400

    Register route without JWT

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
+const mongoose = require('mongoose');
+const config = require('config');
 
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
+            useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log("MongoDB connected...");
+        console.log('MongoDB connected...');
     } catch (error) {
         console.error(error);
         process.exit(1);
